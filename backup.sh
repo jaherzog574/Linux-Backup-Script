85623c9aa640cfd8755b3f5baa41bbcf203f8fd6
speeddemon574 <speeddemon574@gmail.com>
2016-04-21 13:09:50 -0500

    Fixed main backup line, added sys message support
    Leave it to me to upload something that didn't work initially (sorry if you already downloaded this) it should be working now.
    
    This is my first real Linux script (other then practice scripts like "hello world") so I guess it's only natural to make mistakes.
    
    Also added support for sys message, so now the script leaves an entry in the /var/log/message file.

--- backup.sh
+++ backup.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-src="File Name"		# Enter the full path to the directory you wish to backup here.
-dest="Destination location"	# Enter the full path to the directory you wish to backup to here.
-templog="log name"		# Enter the name of the log file that you want attched to your email.
-an=MyBakp-$(date +%m-%d-%Y)_$(date +%H-%M-%S).tgz	# Enter the name you wish the archive to be called (the default option has the time/date in the name).
-email="youremailaddress"	# Enter the email address you wish to send this to (note make shure sendmail is configured and running first).
+# Enter the name of the backup plan you want here.
+name="Backup Name"
 
+# Enter the full path to the directory you wish to backup here.
+src="File Name"	
+
+# Enter the full path to the directory you wish to backup to here.
+dest="Destination location"
+
+# Enter the name of the log file that you want attched to your email.
+templog="log name"		
+
+# Enter the name you wish the archive to be called (the default option has the time/date in the name).
+an=MyBakp-$(date +%m-%d-%Y)_$(date +%H-%M-%S).tgz	
+
+# Enter the email address you wish to send this to (note sendmail must be configured and running for this to work).
+email="youremailaddress"
+
+logger "Backup of $(name) started"
 touch $templog
 echo "Computer name: $(hostname -A)" >> $templog
 echo "Directory backed up: $src" >> $templog
 echo "Started at $(date +%H:%M:%S) $(date +%m/%d/%Y)" >> $templog
 echo "Archive name: $an" >> $templog
 echo "Backup location: $dest" >> $templog
-echo " " >> $templog
-echo "Starting storage space on source partition: " $(df -h $src) >> $templog
-echo " " >> $templog
-echo "Starting storage space on destination partition: " $(df -h $bkp) >> $templog 
-echo " " >> $templog
 echo "########" >> $templog
-echo " " >> $templog
+df -h $src $dest >> $templog
+echo "################" >> $templog
 
-tar -cvZf $dest$an $src >> $templog
+tar -cvpzf $dest$an $src >> $templog
 
 echo " " >> $templog
 echo "############# Changing permissions ############" >> $templog
@@ -28,13 +37,16 @@ chmod -v 644 $dest$an >> $templog
 echo "########" >> $templog
 echo "Backup completed at: $(date +%H:%M:%S) $(date +%m/%d/%Y)" >> $templog
 echo " " >> $templog
-echo "Remaining storage space on source partition: " $(df -h $src) >> $templog
-echo " " >> $templog 
-echo "Remaining storage space on destination partition: " $(df -h $bkp) >> $templog 
-echo " " >> $templog
-echo " " >> $templog
+echo "Backup size of this archive: ">> $templog 
+du -sh $dest$an >> $templog
+echo "All archives of this type: " >> $templog
+du -sh $dest >> $templog
+echo "########" >> $templog
+df -h $src $dest >> $templog
+echo "########" >> $templog
 echo "----- This is the end of the log file -----" >> $templog
+logger "Done backing up $(name)"
 
-echo "Backup on $(hostname -s) has completed at $(date +%H:%M:%S) on $(date +%m-%d-%Y), please check the attached file for details." | mailx -s "Backup complete on $(hostname -s)" -a $templog $email
+echo "$(name) Backup on $(hostname -s) has completed at $(date +%H:%M:%S) on $(date +%m-%d-%Y), please check the attached file for details." | mailx -s "($name) backup complete on $(hostname -s)" -a $templog $email
 
 rm $templog
